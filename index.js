c85ef2cc696f8b1a7d9c3d7593dbec3bb2adfcde
aviral0704 <aviral.asthana0704@gmail.com>
2023-12-13 01:20:04 +0530

    Button keypress response has been added

--- index.js
+++ index.js
@@ -13,4 +13,68 @@ function calculate() {
     } catch (error) {
         document.getElementById('display').value = 'Error';
     }
+<<<<<<< HEAD
 }
+=======
+}
+
+document.addEventListener('keypress',function(event){
+    var ky = event.key;
+    switch(ky){
+        case '0':
+            appendValue(ky);
+            break;
+        case '1':
+            appendValue(ky);
+            break;
+        case '2':
+            appendValue(ky);
+            break;
+        case '3':
+            appendValue(ky);
+            break;
+        case '4':
+            appendValue(ky);
+            break;
+        case '5':
+            appendValue(ky);
+            break;
+        case '6':
+            appendValue(ky);
+            break;
+        case '7':
+            appendValue(ky);
+            break;
+        case '8':
+            appendValue(ky);
+            break;
+        case '9':
+            appendValue(ky);
+            break;
+        case '+':
+            appendValue(ky);
+            break;
+        case '-':
+            appendValue(ky);
+            break;
+        case '*':
+            appendValue(ky);
+            break;
+        case '/':
+            appendValue(ky);
+            break;
+        case '=':
+            calculate();
+            break;
+        case "Enter":
+            calculate();
+            break;
+        case 'C':
+            clearDisplay();
+            break;  
+        default:
+            break;
+    }
+
+})
+>>>>>>> 093229f (Button keypress response has been added)
